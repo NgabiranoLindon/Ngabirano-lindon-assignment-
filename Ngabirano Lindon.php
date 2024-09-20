2c47c57dfde44d69dbe2586f1757987106eec9c1
NgabiranoLindon <ngabiranolindon@gmail.com>
2024-09-20 21:12:50 +0300

    Add files via upload

---
+++ Ngabirano Lindon.php
@@ -0,0 +1,18 @@
+<?php
+$name="Ngabirano Lindon";
+$dateOfBirth="20-8-2000";
+$address="-Tottenham,London";
+$currentYear=2024;
+$yearOfBirth=2000;
+$age=$currentYear-$yearOfBirth;
+
+echo "welcome home ","$name " ," $address.","you are"," $age " ,"years old";
+
+
+
+
+
+
+
+
+?>
